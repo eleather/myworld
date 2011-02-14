6f426d00b2b43e17f3432bcb27724023482d00d8
Emily L <peppermintdove@gmail.com>
2011-02-13 17:36:50 -0800

    brand new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myworld::Application.config.secret_token = 'fd70f6382871b9b0779243aeb4a90acdd278b0b9b7b1e754d862464ce2eff1e5456cc195efd5e48d79a0754f69a861277b350f3e74b3ec53a9e3f7e9267048a0'
