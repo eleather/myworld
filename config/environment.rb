b9777c3a8f68b71fc955a4dedcf5ddaea3d05119
Emily L <peppermintdove@gmail.com>
2011-02-13 18:04:25 -0800

    add haml and sass; configure sass

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Myworld::Application.initialize!
+
+Sass::Plugin.options[:template_location] = './apps/sass'
