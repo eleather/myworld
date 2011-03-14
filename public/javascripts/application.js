28fda1fc3ac94fdfb63e78e448739638380d4c56
Emily L <peppermintdove@gmail.com>
2011-03-13 22:40:12 -0700

    create application.js methods to create buttons that load in the active tab; use those methods in some views

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,27 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Function to load url in active tab.
+function loadUrlInActiveTabInSet(tabs, url) {
+  var selected = tabs.tabs('option', 'selected');
+  tabs.tabs('url', selected, url);
+  tabs.tabs('load', selected);
+}
+
+function loadUrlInActiveTab(url) {
+  loadUrlInActiveTabInSet(jQuery('#tabs'), url);
+}
+
+
+// Buttons that load in active tab.
+function createLoadInActiveTabButtonWithIcon(element, icon, url) {
+  element
+  .button({icons: {primary: icon} })
+  .bind('click', function() {
+    loadUrlInActiveTab(url);
+  });
+}
+
+function createEditInActiveTabButton(element, url) {
+  createLoadInActiveTabButtonWithIcon(element, 'ui-icon-pencil', url);
+}
